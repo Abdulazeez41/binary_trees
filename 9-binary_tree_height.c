d067dc066b23dd9b313c27dae46f910d72acaf5e
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-11-03 01:09:20 +0100

    Added a function that measures the size of a binary tree file

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,9 +8,10 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t c = 0, d = 0;
 	if (tree)
 	{
+		size_t c = 0, d = 0;
+
 		c = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		d = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((c > d) ? c : d);
